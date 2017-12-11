f2f5dd428fd8bd10e08f925ba5bc62a46604a1e2
ArturoRonda <artronluc@alum.us.es>
2017-12-11 10:20:12 +0100

    push necesario

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,4 @@
 package hello;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+	@RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, 
     					   Model model) {
         model.addAttribute("name", name);
