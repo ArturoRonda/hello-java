089ea63576ac79b080a0188488c03499f14c2393
ArturoRonda <artronluc@alum.us.es>
2017-12-11 09:39:38 +0100

    Faltaba un import en el codigo del ejercicio y a mi un ;

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,5 @@
 package hello;
-import GreetingTranslator;
+import GreetingTranslator.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +19,7 @@ public class GreetingController {
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, 
     					   @RequestParam(value="lang", required=false, defaultValue="en") String lang, 
     					   Model model) {
-    	model.addAttribute("hello", GreetingTranslator.sayHelloIn(lang));
+    	model.addAttribute("hello", sayHelloIn(lang));
         model.addAttribute("name", name);
         return "greeting";
     }
